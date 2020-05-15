308d768c072c53b030957a8ae52d832455e7c9b9
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-15 13:58:50 +0300

    Накидал основные состояния родительского класса

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-# гусей "Серый" и "Белый"
+# коз 2 "Рога" и "Копыта"
 # корову "Маньку"
-# овец "Барашек" и "Кудрявый"
-# кур "Ко-Ко" и "Кукареку"
-# коз "Рога" и "Копыта"
-# и утку "Кряква"
+# овец 2 "Барашек" и "Кудрявый"
+# кур 2 "Ко-Ко" и "Кукареку"
+# утку "Кряква"
+# гусей 2 "Серый" и "Белый"
 
 class Creature:
 
-  age = 
+  age = 0 # новорождённое существо
   weight = 0 # у каждого существа есть вес
   hunger = 'hungry' # допустим каждое существо изначально голодное,
-  # но его можно покормить функцией feed() и тогда оно станет сытым ('fed')
+  # его можно покормить методом feed() и тогда оно станет сытым ('fed')
+  says = None # какие звуки издаёт существо
+  location = None
+  vaccine = 'not vaccinated' # существа рождаются непривитыми, им можно делать прививки методом vaccinate()
+  clean_state = 'dirty' # ну существо допустим изначально у нас грязненькое такое
 
-  def feed
 
-
+  def feed(self):
+    self.hunger = 'fed'
+    print('Creature is fed!')
   pass
+
+  def wash(self):
+    self.clean_state = 'clean'
+    print(self, 'is clean now!')
+
+  def vaccinate(self):
+    self.vaccine = 'vaccinated'
+    print(self, 'is vaccinated now! More unlikely to get sick')
 
 class Bird(Creature):
   # крылья
   # несёт яйца
-  # 
+  # можно собрать яйца
   pass
 
 class Animal(Creature):
+  # даёт молоко
+  # можно доить
+
   pass
 
 class Goose(Bird):
@@ -41,5 +57,18 @@ class Sheep(Animal):
 class Goat(Animal):
   pass
 
-class Cow:
+class Cow(Animal):
   pass
+
+print('moo')
+
+creature1 = Creature()
+creature2 = Creature()
+
+print(creature1.vaccine)
+print(creature2.vaccine)
+
+creature2.vaccinate()
+
+print(creature1.vaccine)
+print(creature2.vaccine)
