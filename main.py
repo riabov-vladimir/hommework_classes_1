e1b03e0fc344b8bc5cdb69977eaf3731298a25f7
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-15 12:53:21 +0300

    main.py from Repl.it

---
+++ main.py
@@ -0,0 +1,45 @@
+# гусей "Серый" и "Белый"
+# корову "Маньку"
+# овец "Барашек" и "Кудрявый"
+# кур "Ко-Ко" и "Кукареку"
+# коз "Рога" и "Копыта"
+# и утку "Кряква"
+
+class Creature:
+
+  age = 
+  weight = 0 # у каждого существа есть вес
+  hunger = 'hungry' # допустим каждое существо изначально голодное,
+  # но его можно покормить функцией feed() и тогда оно станет сытым ('fed')
+
+  def feed
+
+
+  pass
+
+class Bird(Creature):
+  # крылья
+  # несёт яйца
+  # 
+  pass
+
+class Animal(Creature):
+  pass
+
+class Goose(Bird):
+  pass
+
+class Duck(Bird):
+  pass
+
+class Chicken(Bird):
+  pass
+
+class Sheep(Animal):
+  pass
+
+class Goat(Animal):
+  pass
+
+class Cow:
+  pass
